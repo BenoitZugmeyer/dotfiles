ddd4b1b714950b50f0a8d2bb37b2fd30da385f36
Benoît Zugmeyer <bzugmeyer@gmail.com>
2016-11-14 22:01:58 +0100

    better deploy script

--- deploy.bash
+++ deploy.bash
@@ -1,65 +1,112 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
-ROOT=$(cd `dirname $0` && pwd)
+ROOT=$(cd $(dirname $0) && pwd)
 SCRIPT=$(basename $0)
 
-IGNORE="
-.gitmodules
-.gitignore
-.netrc
-README.rst
-$SCRIPT
-"
+
+SHORT=fn
+LONG=force,dry-run
+PARSED=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
+eval set -- "$PARSED"
+
 FORCE=false
-if [[ $1 == '-f' ]]; then
-    FORCE=true
+DRY_RUN=false
+
+while true; do
+    case "$1" in
+        -f|--force)
+            FORCE=true
+            shift
+            ;;
+        -n|--dry-run)
+            DRY_RUN=true
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
+
+if [[ ${#} -eq 0 ]] || [[ "${1}" != root ]] && [[ "${1}" != home ]] ; then
+    echo "Usage: $0 [-f | --force] [-n | --dry-run] root | home"
+    exit 1
 fi
 
-inode () {
+function run () {
+    if $DRY_RUN; then
+        echo "#" "${@}"
+    else
+        ${@}
+    fi
+}
+
+function inode () {
     ls -i "$1" | cut -d' ' -f1
 }
 
-hash () {
+function hash () {
     openssl md5 "$1" | tail -c32
 }
 
-lgit () {
+function lgit () {
     git --work-tree="$ROOT" --git-dir="$ROOT/.git" "$@"
 }
 
-while read file; do
+function deploy () {
+    local tree="$1"
+    local target="$2"
 
-    base=$(basename "$file")
-    directory=$(dirname "$file")
+    while read file; do
 
-    [[ "$IGNORE" == *"$base"* ]] && continue
+        local base=$(basename "$file")
+        local directory=$(dirname "$file")
 
-    if ! [[ -d "$HOME/$directory" ]]; then
-        echo "$directory -> creating directory"
-        mkdir -p "$HOME/$directory"
-    fi
-
-    [[ -d "$ROOT/$file" ]] && continue
-
-    if [ -e "$HOME/$file" ]; then
-        if [[ `inode "$ROOT/$file"` != `inode "$HOME/$file"` ]]; then
-            echo -n "$file -> different inode "
-            if $FORCE || [[ `hash "$ROOT/$file"` == `hash "$HOME/$file"` ]]; then
-                echo "same hash, fixing this"
-                rm -f "$HOME/$file" && ln "$ROOT/$file" "$HOME/$file"
-            elif [[ -z $(lgit status --porcelain "$ROOT/$file") ]]; then
-                echo "no modification in repo, copying this"
-                rm -f "$ROOT/$file" && ln "$HOME/$file" "$ROOT/$file"
-            else
-                echo "not same hash, ignoring"
+        if [[ -L "$target/$file" ]]; then
+            if ! [[ -e "$target/$file" ]]; then
+                echo "$file -> broken symlink, removing"
+                run rm "$target/$file"
             fi
         fi
-    else
-        echo "$file -> creating"
-        ln "$ROOT/$file" "$HOME/$file"
-    fi
 
-done < <(lgit ls-files)
+        if ! [[ -d "$target/$directory" ]]; then
+            echo "$directory -> creating directory"
+            run mkdir -p "$target/$directory"
+        fi
 
+        [[ -d "$tree/$file" ]] && continue
+
+        if [ -e "$target/$file" ]; then
+            if [[ $(inode "$tree/$file") != $(inode "$target/$file") ]]; then
+                echo -n "$file -> different inode "
+                if $FORCE || [[ $(hash "$tree/$file") == $(hash "$target/$file") ]]; then
+                    echo "same hash, fixing this"
+                    run rm -f "$target/$file"
+                    run ln "$tree/$file" "$target/$file"
+                elif [[ -z $(lgit status --porcelain "$tree/$file") ]]; then
+                    echo "no modification in repo, copying this"
+                    run rm -f "$tree/$file"
+                    run ln "$target/$file" "$tree/$file"
+                else
+                    echo "not same hash, ignoring"
+                fi
+            fi
+        else
+            echo "$file -> creating"
+            run ln "$tree/$file" "$target/$file"
+        fi
+
+    done < <(cd "$tree"; lgit ls-files)
+}
+
+case "$1" in
+    home) deploy "$ROOT/home" "$HOME" ;;
+    root) deploy "$ROOT/root" / ;;
+esac
